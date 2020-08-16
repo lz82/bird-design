d2236ddf332918fb88a3ccb935b688e0534f0cfa
lz82 <nickylau82@gmail.com>
2020-08-17 00:20:17 +0800

    :construction: init storybook

---
+++ src/stories/button/index.stories.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+// also exported from '@storybook/react' if you can deal with breaking changes in 6.1
+import { Story, Meta } from '@storybook/react/types-6-0';
+import { action } from '@storybook/addon-actions';
+
+import Button, { ButtonProps } from '../../components/button';
+
+export default {
+  title: 'Example/Button',
+  component: Button,
+  argTypes: {
+    btnType: { control: 'btnType' }
+  }
+} as Meta;
+
+const Template: Story<ButtonProps> = (args) => <Button {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+  btnType: 'primary',
+  size: 'lg',
+  text: 'primary button',
+  onClick: action('handle btn click...')
+};
+
+export const Link = Template.bind({});
+Link.args = {
+  btnType: 'link',
+  size: 'lg',
+  text: 'link button',
+  href: 'http://www.baidu.com'
+};
