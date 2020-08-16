347ea94f6b9eb23aa9cfac04508aa845333e2038
lz82 <nickylau82@gmail.com>
2020-08-16 17:03:56 +0800

    :construction: modify preview: icon

--- src/views/icon/index.tsx
+++ src/views/icon/index.tsx
@@ -4,6 +4,7 @@ import Icon from '../../components/icon';
 export default function index() {
   return (
     <div>
+      <h2>Icon</h2>
       <Icon theme="primary" icon="coffee" size="10x" />
       <Icon theme="danger" icon="coffee" size="10x" />
       <Icon theme="warning" icon="coffee" size="10x" />
